73636c84e7dadb09d4b38b6702ce9f960173f356
Thomas Watters <thomaswatters@gmail.com>
2015-06-09 16:59:15 -0400

    Made changes to surveyboom service to support CreateSurvey

--- SurveyBoomService/SurveyBoomService/SurveyBoomModel.Designer.cs
+++ SurveyBoomService/SurveyBoomService/SurveyBoomModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\thomaswatters\Dropbox\School\COP4331\SurveyBoom\SurveyBoomService\SurveyBoomService\SurveyBoomModel.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Users\Thomas\Dropbox\School\COP4331\SurveyBoom\SurveyBoomService\SurveyBoomService\SurveyBoomModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
